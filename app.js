31690ed1e58a38132ac41bf34304fbdbbec8f01c
manohar <manumanohar1527@gmail.com>
2020-04-03 07:26:00 -0700

    added eventListerner

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const endDate = document.querySelector('input[name="endDate"]');
 console.log(endDate);
 const clock = document.querySelector('.clock');
-console.log(clock);
+//console.log(clock);
+
+// adding eventListener whenever the changes made to the input..
+
+endDate.addEventListener('change', (e) => {
+    console.log(endDate.value);
+    let temp = new Date(endDate.value);
+    console.log(temp);
+});
