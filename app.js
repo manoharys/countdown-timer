191f3d29ee434f24d049ae14f4257f2b8971ba1c
manohar <manumanohar1527@gmail.com>
2020-04-03 10:12:55 -0700

    added time left function

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const endDate = document.querySelector('input[name="endDate"]');
-console.log(endDate);
+
 const clock = document.querySelector('.clock');
-//console.log(clock);
+
 
 // adding eventListener whenever the changes made to the input..
 
 endDate.addEventListener('change', (e) => {
-    console.log(endDate.value);
+   // console.log(endDate.value);
     let temp = new Date(endDate.value);
     console.log(temp);
+    console.log(endClock(temp));
+    //console.log(startClock(temp));
 });
+
+function startClock(d) {
+   // endClock(d);
+}
+
+function endClock(d) {
+    let currentDate = new Date();
+    console.log(currentDate);
+    console.log(Date.parse(currentDate));
+    console.log(Date.parse(d));
+    let t = Date.parse(d) - Date.parse(currentDate);
+    console.log(t);
+
+    let  seconds = Math.floor((t/1000)%60);
+    let  minutes = Math.floor((t/1000/60)%60);
+    let  hours = Math.floor((t/(1000*60*60))%24);
+    let  days= Math.floor((t/(1000*60*60*24)));
+
+      return{
+          "total":t,
+          "days":days,
+          "hours":hours,
+          "minutes":minutes,
+          "seconds":seconds
+      };
+}
