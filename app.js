10d8c6a8ac050c07caa60589b75a64fa98968245
manohar <manumanohar1527@gmail.com>
2020-04-03 11:09:30 -0700

    added time interval

--- app.js
+++ app.js
@@ -2,37 +2,48 @@ const endDate = document.querySelector('input[name="endDate"]');
 
 const clock = document.querySelector('.clock');
 
+let timeInterval;
+let stop = true;
 
 // adding eventListener whenever the changes made to the input..
 
 endDate.addEventListener('change', (e) => {
     // console.log(endDate.value);
+    e.preventDefault();
     let temp = new Date(endDate.value);
-    // console.log(temp);
-    // let k=(endClock(temp));
-    //console.log(startClock(temp));
+    clearInterval(timeInterval);
     startClock(temp);
-
+    stop = false;
 });
 
 function startClock(d) {
-    let timeLeft = endClock(d);
-    //console.log(timeLeft.days);
-    for (let pro in timeLeft) {
-        console.log(pro, timeLeft[pro]);
-        let element = document.querySelector("." + pro);
-        if (element) {
-            console.log(element);
-            element.innerHTML = timeLeft[pro] + "    ";
+    function updateCounter() {
+        let timeLeft = endClock(d);
+        //console.log(timeLeft.days);
+        //Time stops if the input date is zero or negative
+        if(timeLeft.total <= 0){
+            stop = false;
         }
+        for (let pro in timeLeft) {
+            //console.log(pro, timeLeft[pro]);
+            let element = document.querySelector("." + pro);
+            if (element) {
+                //console.log(element);
+                element.innerHTML = timeLeft[pro] + "    ";
+            }
+        }
+    }
+    if (stop) {
+        timeInterval = setInterval(updateCounter, 1000);
+    }else{
+        stop = false;
+        clearInterval(timeInterval);
     }
 }
 
 function endClock(d) {
     let currentDate = new Date();
-    // console.log(currentDate);
-    // console.log(Date.parse(currentDate));
-    //console.log(Date.parse(d));
+
     let t = Date.parse(d) - Date.parse(currentDate);
     //console.log(t);
 
